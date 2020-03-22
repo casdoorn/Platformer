1cc8386d04d782324004b157f3b8a9f4b869a746
Cas Doornkamp <cjdoornkamp@gmail.com>
2020-03-22 21:59:11 +0100

    decoupled loading of scene from draw loop, added scene navigation with WASD

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,17 @@ extern crate ai_behavior;
 extern crate tiled;
 use std::path::Path;
 use piston_window::*;
+use piston_window::Button::Keyboard;
 
 fn main() {
     let map = tiled::parse_file(&Path::new("assets/maps/map-2.tmx")).unwrap();
 
-    let (width, height) = (600, 600);
+    // player pos and view scale
+    let scale = 1.0;
+    let (mut x_view, mut y_view) = (0.0, 0.0);
+
+    // window settings
+    let (width, height) = (900, 600);
     let opengl = OpenGL::V3_2;
     let mut window: PistonWindow = WindowSettings::new("piston: tiled", [width, height])
         .exit_on_esc(true)
@@ -19,10 +25,11 @@ fn main() {
     let tileset = map.get_tileset_by_gid(1).unwrap();
     let tile_width = tileset.tile_width;
     let tile_height = tileset.tile_height;
+    // println!("tilesetmap: {:?}", map);
 
     let ref mut texture_context = window.create_texture_context();
     let tileset_src = String::from("assets/maps/") + &tileset.images[0].source;
-    println!("tileset: {}", tileset_src);
+    // println!("tileset: {}", tileset_src);
     
     let tilesheet = tileset_src;
     let tilesheet = Texture::from_path(
@@ -32,44 +39,110 @@ fn main() {
         &TextureSettings::new(),
     ).unwrap();
 
-    let (width, _) = tilesheet.get_size();
     let layer: &tiled::Layer = &map.layers[0];
-    let image = Image::new();
 
+    // load scene once
+    let mut level_src: Vec<[f64;4]> = Vec::new();
+    let mut level_dest: Vec<[f64;4]> = Vec::new();
+    
+    // calculate view dimensions (needed for only loading an drawing the needed tiles)
+    // TODO(Cas): fix dit
+    // drawing gaat alleen goed als de scale of translation hele getallen zijn
+    // let x_min = (x_view - (width as f64/2 as f64) - tile_width as f64) as i32;
+    // let x_max = (x_view + (width as f64/2 as f64) + tile_width as f64) as i32;
+    // let y_min = (y_view - (height as f64/2 as f64) - tile_height as f64) as i32;
+    // let y_max = (y_view + (height as f64/2 as f64) + tile_height as f64) as i32;
+    // let x_step = (tile_width as f64/scale) as usize;
+    // let y_step = (tile_height as f64/scale) as usize;
+
+    // for (y_tile, y_world) in (y_min..y_max).step_by(y_step).enumerate(){
+    //     for (x_tile, x_world) in (x_min..x_max).step_by(x_step).enumerate() {
+    //         let tile = get_tile(&layer, x_world, y_world, tile_width, tile_height);
+
+    for (y, row) in layer.tiles.iter().enumerate().clone() {
+        for (x, &tile) in row.iter().enumerate() {
+            
+            if tile == 0 {
+                continue;
+            }
+            
+            let (width, _) = tilesheet.get_size();
+            let tile = tile - 1; // tiled counts from 1
+
+
+            // create mapping of source blocks. level_src will be mapped onto level_dest
+            // describe tile source block
+            level_src.push([
+                (tile % (width / tile_width) * tile_width) as f64,
+                (tile / (width / tile_height) * tile_height) as f64,
+                tile_width as f64,
+                tile_height as f64,
+            ]);
+            
+            // describe destination of tile in pixels
+            level_dest.push([
+                (x as u32* tile_width) as f64,// - x_view as f64,
+                (y as u32* tile_height) as f64,// - y_view as f64,
+                tile_width as f64,
+                tile_height as f64
+            ]);
+        }
+    }
+
+
+    // draw/event loop
     while let Some(e) = window.next() {
+        if let Some(Keyboard(key)) = e.press_args() {
+            // deltas have to be whole numbers, otherwise the drawing will have glitches
+            match key{
+                Key::D => x_view += 2.0,
+                Key::A => x_view -= 2.0,
+                Key::W => y_view -= 2.0, // up is negative directiion
+                Key::S => y_view += 2.0,
+                _ => {}
+            }
+        }
+
+
         window.draw_2d(&e, |c, g, _| {
             clear([0.5; 4], g);
 
-            for (y, row) in layer.tiles.iter().enumerate().clone() {
-                for (x, &tile) in row.iter().enumerate() {
-                    if tile == 0 {
-                        continue;
+            // scale and translate scene
+            let trans = c.transform.scale(
+                scale as f64,
+                scale as f64,
+            ).trans(
+                -x_view,
+                -y_view
+            );
+            // give normal color for level. Otherwise the level will be tinted
+            let color = [1.0; 4];
+            
+            // draw each tile
+            for (&src,&dest) in level_src.iter().zip(level_dest.iter()){
+                g.tri_list_uv(
+                    &DrawState::default(),
+                    &color,
+                    &tilesheet,
+                    |f| {
+                        f(&triangulation::rect_tri_list_xy(trans, dest),
+                        &triangulation::rect_tri_list_uv(&tilesheet, src))
                     }
-
-                    let tile = tile - 1; // tiled counts from 1
-
-                    // rect of the particular tile in the tilesheet
-                    let src_rect = [
-                        (tile % (width / tile_width) * tile_width) as f64,
-                        (tile / (width / tile_height) * tile_height) as f64,
-                        tile_width as f64,
-                        tile_height as f64,
-                    ];
-
-                    let trans = c.transform.trans(
-                        x as f64 * tile_width as f64,
-                        y as f64 * tile_height as f64,
-                    );
-
-                    image.src_rect(src_rect).draw(
-                        &tilesheet,
-                        &DrawState::default(),
-                        trans,
-                        g,
-                    );
-                }
+                );
             }
         });
     }
 }
 
+fn get_tile(layer: &tiled::Layer, x: i32, y: i32, tile_width: u32, tile_height: u32) -> u32 {
+ 
+    let y_max = layer.tiles.len();
+    let x_max = layer.tiles[0].len();
+    let x = (x / tile_width as i32) as usize;
+    let y = (y / tile_height as i32) as usize;
+    if x>= 0 && x < x_max && y >= 0 && y < y_max {
+        layer.tiles[y][x]
+    }else{
+        0
+    }
+}
