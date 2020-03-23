476afff028102b3e38756091d864e342482421cb
mrlukasbos <mrlukasbos@gmail.com>
2020-03-23 20:52:04 +0100

    add new map

---
+++ assets/maps/tileset2.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="tileset2" tilewidth="32" tileheight="32" tilecount="768" columns="32">
+ <image source="tileset2.png" width="1024" height="768"/>
+ <tile id="420">
+  <objectgroup draworder="index" id="3">
+   <object id="5" x="16" y="16"/>
+  </objectgroup>
+ </tile>
+</tileset>
